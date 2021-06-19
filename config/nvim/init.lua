6e85ece807eedd19d872ce743f4e2921a93fc90a
Bartosz Burclaf <burclaf@gmail.com>
2021-06-19 22:57:36 +0200

    add missing plugins

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,7 @@
+-- global aliases
+local cmd = vim.cmd
+local g = vim.g
+
 -- globals
 require "settings"
 require "mappings"
@@ -11,3 +15,14 @@ require "plg_treesitter"
 -- color scheme
 local base16 = require "base16"
 base16(base16.themes["spacemacs"], true)
+
+-- blankline
+
+g.indentLine_enabled = 1
+g.indent_blankline_char = "▏"
+
+g.indent_blankline_filetype_exclude = {"help", "terminal", "dashboard"}
+g.indent_blankline_buftype_exclude = {"terminal"}
+
+g.indent_blankline_show_trailing_blankline_indent = false
+g.indent_blankline_show_first_indent_level = false
