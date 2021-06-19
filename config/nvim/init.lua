6d5376292cdedeb6bda6bd126328294fdae35cf3
Bartosz Burclaf <burclaf@gmail.com>
2021-06-19 22:43:30 +0200

    update colorscheme

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,8 +1,13 @@
+-- globals
 require "settings"
 require "mappings"
 require "plugins"
---
----- plugins configuration
+
+-- plugins configuration
 require "plg_lsp"
 require "plg_completion"
 require "plg_treesitter"
+
+-- color scheme
+local base16 = require "base16"
+base16(base16.themes["spacemacs"], true)
