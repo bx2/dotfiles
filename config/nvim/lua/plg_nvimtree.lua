9180d7303e0de3f8fd8a43e7b5ca8f7f15b85fda
Bartosz Burclaf <burclaf@gmail.com>
2021-06-21 14:33:52 +0200

    handle netrw properly

--- config/nvim/lua/plg_nvimtree.lua
+++ config/nvim/lua/plg_nvimtree.lua
@@ -1,9 +1,9 @@
 local g = vim.g
 
 g.nvim_tree_side = "left"
-g.nvim_tree_width = 30
+g.nvim_tree_width = 35
 g.nvim_tree_ignore = {}
-g.nvim_tree_auto_open = 0
+g.nvim_tree_auto_open = 1
 g.nvim_tree_auto_close = 0
 g.nvim_tree_quit_on_open = 0
 g.nvim_tree_follow = 1
@@ -14,6 +14,8 @@ g.nvim_tree_root_folder_modifier = ":t"
 g.nvim_tree_tab_open = 0
 g.nvim_tree_allow_resize = 1
 g.nvim_tree_gitignore = 1
+g.nvim_tree_disable_netrw = 1
+g.nvim_tree_hijack_netrw = 1
 
 g.nvim_tree_show_icons = {
     git = 0,
